2721a4e0f763fc549170784e0585179529ab9fe6
Beth Caruana <bethanncaruana@gmail.com>
2024-06-19 16:19:41 -0400

    45 minutes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -23,13 +23,18 @@ function Navbar(){
         }
     };
 
+    //prevents the sign-up button from re-appearing when refresh in mobile mode
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         Dolphin 
                         <FontAwesomeIcon icon={faPersonSwimming} />
                     </Link>
